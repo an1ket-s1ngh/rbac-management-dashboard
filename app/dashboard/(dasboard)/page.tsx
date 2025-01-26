76d2ee9a9c8d4c333af4b729e7c33307cf5bf5de
Aniket Singh <aniket.sa@outlook.com>
2025-01-26 07:47:09 +0530

    Update dashboard layout and title for clarity

--- app/dashboard/(dasboard)/page.tsx
+++ app/dashboard/(dasboard)/page.tsx
@@ -5,9 +5,8 @@ import { readMemberNameByID } from '../tasks/actions';
 export default function Dashboard() {
 
     return (
-		<div style={{ fontSize: '2em' }}>
-			<h1 style={{ fontSize: '2.5em' }}>Your Dashboard</h1>
-			<p>This dashboard provides an overview of the Role-Based Access Control (RBAC) system. Here you can manage roles, users and tasks effectively.</p>
+		<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90%' }}>
+			<h1 style={{ fontSize: '2.5em' }}>Your One Stop RBAC Dashboard!</h1>
 			<div>
 			</div>
 		</div>
